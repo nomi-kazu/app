837237559ba6e4850a94bd6180fe2c17279e55e3
nomi-kazu <nomikazyu@gmail.com>
2021-10-07 22:14:06 +0900

    delete: indexを初期化

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,109 +1,11 @@
 <template>
-  <v-container fluid>
-    <v-card
-      flat
-      tile
-      color="transparent"
-    >
-      <v-card-title>
-        nuxt-i18nの検証
-      </v-card-title>
-      <v-card-text>
-        <v-simple-table dense>
-          <template v-slot:default>
-            <thead>
-              <tr>
-                <th>en</th>
-                <th>ja</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr
-                v-for="(path, i) in ['signup', 'login']"
-                :key="`path-${i}`"
-              >
-                <td>{{ path }}</td>
-                <td>{{ $t(`title.${path}`) }}</td>
-              </tr>
-            </tbody>
-          </template>
-        </v-simple-table>
-      </v-card-text>
-      <v-card-title>
-        Usersテーブルの取得
-      </v-card-title>
-      <v-card-text>
-        <v-simple-table dense>
-          <template
-            v-if="users.length"
-            v-slot:default
-          >
-            <thead>
-              <tr>
-                <th
-                  v-for="(key, i) in userKeys"
-                  :key="`key-${i}`"
-                >
-                  {{ key }}
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr
-                v-for="(user, i) in users"
-                :key="`user-${i}`"
-              >
-                <td>{{ user.id }}</td>
-                <td>{{ user.name }}</td>
-                <td>{{ user.email }}</td>
-                <td>{{ dateFormat(user.created_at) }}</td>
-              </tr>
-            </tbody>
-          </template>
-          <template v-else>
-            ユーザーが存在しません
-          </template>
-        </v-simple-table>
-      </v-card-text>
-      <v-card-title>
-        Vuetifyの導入（オリジナルカラーの確認）
-      </v-card-title>
-      <v-card-text>
-        <v-btn
-          v-for="(color, i) in colors"
-          :key="`color-${i}`"
-          :color="color"
-          class="mr-2"
-        >
-          {{ color }}
-        </v-btn>
-      </v-card-text>
-    </v-card>
-  </v-container>
+  <div>
+    index.vue
+  </div>
 </template>
 
 <script>
 export default {
-  async asyncData ({ $axios }) {
-    let users = []
-    await $axios.$get('/api/v1/users').then(res => (users = res))
-    const userKeys = Object.keys(users[0] || {})
-    return { users, userKeys }
-  },
-  data () {
-    return {
-      colors: ['primary', 'info', 'success', 'warning', 'error', 'background']
-    }
-  },
-  computed: {
-    dateFormat () {
-      return (date) => {
-        const dateTimeFormat = new Intl.DateTimeFormat(
-          'ja', { dateStyle: 'medium', timeStyle: 'short' }
-        )
-        return dateTimeFormat.format(new Date(date))
-      }
-    }
-  }
+
 }
 </script>
