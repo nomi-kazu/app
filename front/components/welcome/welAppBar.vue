92e29837eb24e089912ad9be95a0874c31201380
nomi-kazu <nomikazyu@gmail.com>
2021-10-08 19:21:20 +0900

    feat: addEventListenerでスクロールを検知してツールバーの色を変化させる

--- front/components/welcome/welAppBar.vue
+++ front/components/welcome/welAppBar.vue
@@ -1,8 +1,17 @@
 <template>
   <v-app-bar
     app
-    dark
+    :dark="!isScrollPoint"
+    :height="appBarHeight"
+    :color="toolbarStyle.color"
+    :elevation="toolbarStyle.elevation"
   >
+    <app-logo
+      @click.native="goTo('scroll-top')"
+    />
+    <v-toolbar-title>
+      {{ appName }}
+    </v-toolbar-title>
     <v-spacer />
 
     <v-toolbar-items class="ml-2">
@@ -10,14 +19,11 @@
         v-for="(menu, i) in menus"
         :key="`menu-btn-${i}`"
         text
+        @click="goTo(menu.title)"
       >
         {{ $t(`menus.${menu.title}`) }}
       </v-btn>
     </v-toolbar-items>
-    <app-logo />
-    <v-toolbar-title>
-      {{ appName }}
-    </v-toolbar-title>
   </v-app-bar>
 </template>
 
@@ -32,11 +38,41 @@ export default {
     menus: {
       type: Array,
       default: () => []
+    },
+    imgHeight: {
+      type: Number,
+      default: 0
     }
   },
-  data ({ $config: { appName } }) {
+  data ({ $config: { appName }, $store }) {
     return {
-      appName
+      appName,
+      scrollY: 0,
+      appBarHeight: $store.state.styles.beforeLogin.appBarHeight
+    }
+  },
+  computed: {
+    isScrollPoint () {
+      return this.scrollY > (this.imgHeight - this.appBarHeight)
+    },
+    toolbarStyle () {
+      const color = this.isScrollPoint ? 'white' : 'transparent'
+      const elevation = this.isScrollPoint ? 4 : 0
+      return { color, elevation }
+    }
+  },
+  mounted () {
+    window.addEventListener('scroll', this.onScroll)
+  },
+  beforeDestroy () {
+    window.removeEventListener('scroll', this.onScroll)
+  },
+  methods: {
+    onScroll () {
+      this.scrollY = window.scrollY
+    },
+    goTo (id) {
+      this.$vuetify.goTo(`#${id}`)
     }
   }
 }
