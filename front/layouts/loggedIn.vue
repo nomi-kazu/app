31236965e4cbc637b812954c9d2c94249027f2e2
nomi-kazu <nomikazyu@gmail.com>
2021-10-09 01:00:08 +0900

    feat: ログイン後のツールバーを作成

---
+++ front/layouts/loggedIn.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <logged-in-app-bar />
+    <v-main>
+      <nuxt />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import loggedInAppBar from '~/components/loggedIn/header/loggedInAppBar'
+
+export default {
+  loggedInAppBar
+}
+</script>
