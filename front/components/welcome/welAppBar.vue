36ed035945bf91164c2a6f6c2c26e827021eb0ee
nomi-kazu <nomikazyu@gmail.com>
2021-10-08 18:56:54 +0900

    feat: メニューボタンを作成

--- front/components/welcome/welAppBar.vue
+++ front/components/welcome/welAppBar.vue
@@ -1,10 +1,43 @@
 <template>
-  <div>
-    welAppBar.vue
-  </div>
+  <v-app-bar
+    app
+    dark
+  >
+    <v-spacer />
+
+    <v-toolbar-items class="ml-2">
+      <v-btn
+        v-for="(menu, i) in menus"
+        :key="`menu-btn-${i}`"
+        text
+      >
+        {{ $t(`menus.${menu.title}`) }}
+      </v-btn>
+    </v-toolbar-items>
+    <app-logo />
+    <v-toolbar-title>
+      {{ appName }}
+    </v-toolbar-title>
+  </v-app-bar>
 </template>
 
 <script>
+import appLogo from '~/components/ui/appLogo'
+
 export default {
+  components: {
+    appLogo
+  },
+  props: {
+    menus: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data ({ $config: { appName } }) {
+    return {
+      appName
+    }
+  }
 }
 </script>
