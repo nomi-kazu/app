680ad11f326c1834a14b8be4755dc0c6b0012e56
nomi-kazu <nomikazyu@gmail.com>
2021-10-08 19:40:55 +0900

    feat: ウェルカムページをレスポンシブデザインにした

--- front/components/welcome/welAppBar.vue
+++ front/components/welcome/welAppBar.vue
@@ -9,30 +9,66 @@
     <app-logo
       @click.native="goTo('scroll-top')"
     />
-    <v-toolbar-title>
+    <v-toolbar-title
+      class="hidden-modile-and-down"
+    >
       {{ appName }}
     </v-toolbar-title>
     <v-spacer />
 
-    <v-toolbar-items class="ml-2">
+    <v-toolbar-items class="ml-2 hidden-ipad-and-down">
       <v-btn
         v-for="(menu, i) in menus"
         :key="`menu-btn-${i}`"
         text
+        :class="{ 'hidden-sm-and-down': (menu.title === 'about') }"
         @click="goTo(menu.title)"
       >
         {{ $t(`menus.${menu.title}`) }}
       </v-btn>
     </v-toolbar-items>
+    <signup-link />
+    <login-link />
+    <v-menu
+      bottom
+      nudge-left="110"
+      nudge-width="100"
+    >
+      <template #activator="{ on }">
+        <v-app-bar-nav-icon
+          class="hidden-ipad-and-up"
+          v-on="on"
+        />
+      </template>
+      <v-list
+        dense
+        class="hidden-ipad-and-up"
+      >
+        <v-list-item
+          v-for="(menu, i) in menus"
+          :key="`menu-list-${i}`"
+          exact
+          @click="goTo(menu.title)"
+        >
+          <v-list-item-title>
+            {{ $t(`menus.${menu.title}`) }}
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </v-app-bar>
 </template>
 
 <script>
 import appLogo from '~/components/ui/appLogo'
+import signupLink from '~/components/beforeLogin/signupLink'
+import loginLink from '~/components/beforeLogin/loginLink'
 
 export default {
   components: {
-    appLogo
+    appLogo,
+    signupLink,
+    loginLink
   },
   props: {
     menus: {
