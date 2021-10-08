96375b248a58901727bb3ad79adcde6e938264cb
nomi-kazu <nomikazyu@gmail.com>
2021-10-08 23:57:08 +0900

    feat: 親子コンポーネント間の双方向バインディングを実装

--- front/pages/signup.vue
+++ front/pages/signup.vue
@@ -3,13 +3,19 @@
     <v-form
       v-model="isValid"
     >
-      <user-form-name />
-      <user-form-email />
-      <user-form-password />
+      <user-form-name
+        :name.sync="params.user.name"
+      />
+      <user-form-email
+        :email.sync="params.user.email"
+      />
+      <user-form-password
+        :password.sync="params.user.password"
+      />
       <v-btn
         :disabled="!isValid"
         block
-        color="myblue"
+        color="black"
         class="white--text"
       >
         登録する
@@ -34,7 +40,8 @@ export default {
   layout: 'beforeLogin',
   data () {
     return {
-      isValid: false
+      isValid: false,
+      params: { user: { name: '', email: '', password: '' } }
     }
   }
 }
